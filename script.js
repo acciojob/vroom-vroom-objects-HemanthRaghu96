eb04cc7af7db675cd6507b44d0726233053293c2
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-11-29 18:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // Complete the js code
-function Car(make, model) {}
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+  getMakeModel() {
+    return this.make + " " + this.model;
+  }
+}
 
-function SportsCar(make, model, topSpeed) {}
+class SportsCar extends Car {
+  constructor(make, model, topSpeed) {
+    super(make, model);
+    this.topSpeed = topSpeed;
+  }
+  getTopSpeed() {
+    return this.topSpeed;
+  }
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+console.log(car.getMakeModel());
+
+console.log(car.getTopSpeed()); 
+
 
 // Do not change the code below
 window.Car = Car;
